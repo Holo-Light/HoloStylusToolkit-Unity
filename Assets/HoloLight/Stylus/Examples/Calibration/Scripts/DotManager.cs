05bef6792b912bf47456bad8214316d51e7fec1b
p.landgraf <p.landgraf@holo-light.com>
2019-03-05 15:26:54 +0100

    Add The possibility to change the Tip Pointer as well as fixed the coloring scene

---
+++ Assets/HoloLight/Stylus/Examples/Calibration/Scripts/DotManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotManager : MonoBehaviour {
+    private GameObject Dot;
+	// Use this for initialization
+	void Start ()
+	{
+	    Dot=GameObject.Find("Tip");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+    public void TipOnOff()
+    {
+        Dot.GetComponent<Renderer>().enabled=!Dot.GetComponent<Renderer>().enabled;
+    }
+
+    public void TipScaleUp()
+    {
+        Dot.transform.localScale = Dot.transform.localScale * 1.5f;
+    }
+    public void TipScaleDown()
+    {
+        Dot.transform.localScale = Dot.transform.localScale * (1.0f/1.5f);
+    }
+}
